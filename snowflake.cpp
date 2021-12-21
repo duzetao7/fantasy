9ebe454bb6762b1fed75aa2d5567fc87a6beb550
DuZeTao7 <a18211265359@icloud.com>
2021-12-22 00:17:39 +0800

    add 1.snowflake

---
+++ snowflake.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+int main()
+{
+	printf("              *                   *              \n");
+	printf("               *                 *               \n");
+	printf("                *               *                \n");
+	printf("                 *             *                 \n");
+	printf("                  *           *                  \n");
+	printf("                   *         *                   \n");
+	printf("                    *       *                    \n");
+	printf("                     *     *                     \n");
+	printf("                      *   *                      \n");
+	printf("                       * *                       \n");
+	printf("* * * * * * * * * * * * * * * * * * * * * * * * *\n");
+	printf("                       * *                       \n");
+	printf("                      *   *                      \n");
+	printf("                     *     *                     \n");
+	printf("                    *       *                    \n");
+	printf("                   *         *                   \n");
+	printf("                  *           *                  \n");
+	printf("                 *             *                 \n");
+	printf("                *               *                \n");
+	printf("               *                 *               \n");
+	printf("              *                   *              \n");
+
+	return 0;
+}
